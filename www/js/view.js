044412f385d0aa32b8ad9da14e022a8049f9f046
FedeBer <fedegeek@gmail.com>
2016-03-19 08:20:59 +0100

    Reformatted code, removed unused foundation css

--- www/js/view.js
+++ www/js/view.js
@@ -22,21 +22,21 @@ Login.prototype.constructor = Login;
 Login.prototype.print = function () {
     this.clean();
     this.inputUserName = document.createElement("input");
-    this.inputUserName.setAttribute("type","text");
-    this.inputUserName.setAttribute("placeholder","username");
+    this.inputUserName.setAttribute("type", "text");
+    this.inputUserName.setAttribute("placeholder", "username");
     this.inputUserName.className += "centered";
     this.inputPassword = document.createElement("input");
-    this.inputPassword.setAttribute("type","password");
-    this.inputPassword.setAttribute("placeholder","password");
+    this.inputPassword.setAttribute("type", "password");
+    this.inputPassword.setAttribute("placeholder", "password");
     this.inputPassword.className += "centered";
     var loginButton = document.createElement("button");
     loginButton.className += "button centered";
     loginButton.innerHTML = "Login";
-    loginButton.addEventListener("click",app.makeLogin);
+    loginButton.addEventListener("click", app.makeLogin);
     var signUpButton = document.createElement("button");
     signUpButton.className += "button centered";
     signUpButton.innerHTML = "Sign up";
-    signUpButton.addEventListener("click",app.showSignUp);
+    signUpButton.addEventListener("click", app.showSignUp);
     this.mainFrame.appendChild(this.inputUserName);
     this.mainFrame.appendChild(this.inputPassword);
     this.mainFrame.appendChild(loginButton);
@@ -58,22 +58,22 @@ SignUp.prototype.print = function () {
     var sectionContainer = document.createElement("section");
     sectionContainer.className += "grid-container mediumMarginTop";
     this.inputUserName = document.createElement("input");
-    this.inputUserName.setAttribute("type","text");
-    this.inputUserName.setAttribute("placeholder","username");
+    this.inputUserName.setAttribute("type", "text");
+    this.inputUserName.setAttribute("placeholder", "username");
     this.inputEmail = document.createElement("input");
-    this.inputEmail.setAttribute("type","email");
-    this.inputEmail.setAttribute("placeholder","e-mail");
+    this.inputEmail.setAttribute("type", "email");
+    this.inputEmail.setAttribute("placeholder", "e-mail");
     this.inputPassword = document.createElement("input");
-    this.inputPassword.setAttribute("type","password");
-    this.inputPassword.setAttribute("placeholder","password");
+    this.inputPassword.setAttribute("type", "password");
+    this.inputPassword.setAttribute("placeholder", "password");
     var signUpButton = document.createElement("button");
     signUpButton.className += "button";
     signUpButton.innerHTML = "Sign Up";
-    signUpButton.addEventListener("click",app.makeSignUp);
+    signUpButton.addEventListener("click", app.makeSignUp);
     var backButton = document.createElement("button");
     backButton.className += "button";
     backButton.innerHTML = "Back";
-    backButton.addEventListener("click",app.showLogin);
+    backButton.addEventListener("click", app.showLogin);
     sectionGridBlock.appendChild(sectionContainer);
     sectionContainer.appendChild(this.inputUserName);
     sectionContainer.appendChild(this.inputEmail);
@@ -98,28 +98,28 @@ HomePage.prototype.print = function () {
     var logoutButton = document.createElement("button");
     logoutButton.innerHTML = "Logout";
     logoutButton.id = "logoutButton";
-    logoutButton.addEventListener("click",app.makeLogOut);
+    logoutButton.addEventListener("click", app.makeLogOut);
     this.textArea = document.createElement("textarea");
     this.textArea.id = "postInputText";
     var submitButton = document.createElement("button");
     submitButton.innerHTML = "Submit";
     submitButton.id = "submitButton";
-    submitButton.addEventListener("click",app.makePost);
+    submitButton.addEventListener("click", app.makePost);
     var line = document.createElement("hr");
     this.mainFrame.appendChild(logoutButton);
     this.mainFrame.appendChild(this.textArea);
     this.mainFrame.appendChild(submitButton);
     this.mainFrame.appendChild(line);
-        for (var key in this.posts) {
+    for (var key in this.posts) {
         var postView = document.createElement("article");
         var post = this.posts[key];
-        postView.innerHTML = post.get("Author").getUsername()+" ha scritto: " + post.get("text");
+        postView.innerHTML = post.get("Author").getUsername() + " ha scritto: " + post.get("text");
         postView.className += "post";
         this.mainFrame.appendChild(postView);
     }
 };
 
-PluginPage = function() {
+PluginPage = function () {
     View.call();
 };
 
